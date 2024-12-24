71ddec932213359b9060f8e4d854793777f39b6a
djberenberg <danielberenberg@gmail.com>
2024-12-24 14:35:43 +0100

    scripts

--- src/dataset_server/functional/_read_jsonl.py
+++ src/dataset_server/functional/_read_jsonl.py
@@ -1,9 +1,10 @@
+import gzip
 import json
 
 from dataset_server.typing import JSONSerializeable
 
 
-def read_jsonl(filename: str) -> list[JSONSerializeable]:
+def read_jsonl(filename: str, gzip_compressed: bool = False) -> list[JSONSerializeable]:
 
-    with open(filename, "r") as f:
+    with (gzip.open if gzip_compressed else open)(filename, "rt") as f:
         return [json.loads(ln) for ln in f]
